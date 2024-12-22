ac46bf45f8362b867a215ef3dabff1f1618d9083
Wallzer <35204495+Wallzer@users.noreply.github.com>
2024-12-22 15:59:30 +0200

    d

--- pomodoro.py
+++ pomodoro.py
@@ -214,4 +214,3 @@ canvas_settings = CanvasButton(canvas, 360, 300, SETTINGS_IMG, show_setings)
 canvas_back = CanvasButton(canvas_setttings, 360, 300, SETTINGS_IMG, show_setings)
 # Start the main loop
 root.mainloop()
-print('hehe')
